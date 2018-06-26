06b64bcfa4890d471e03d1c59c9b41d7acde61ef
Spyte <40525440+Spajciuch@users.noreply.github.com>
2018-06-26 22:55:55 +0200

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -2,7 +2,7 @@
 const Discord = require("discord.js");
 module.exports.run = async (client, message, args) => {
   const m = await message.channel.send("Ping? <a:loading:460475336736374787>");
-  m.edit(`<a:loading:460475336736374787> Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+  m.edit(`<a:loading:461273022347870223> Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
 }
 module.exports.help = {
   name: "ping"
