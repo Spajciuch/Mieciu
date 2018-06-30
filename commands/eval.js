3a54c28e3d86ea04401986a0cb55c7114f36f169
Spyte <40525440+Spajciuch@users.noreply.github.com>
2018-06-30 13:05:22 +0200

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -2,7 +2,6 @@ const Discord = require("discord.js");
 const config = require(`../config.json`)
 module.exports.run = async (client, message, args) => {
   if(message.author.id !== '367390191721381890') return message.reply("Nie masz uprawnień")
-			if(!result) return console.log("Błąd Eval")
       let embed = new Discord.RichEmbed()
       .setTitle("Eval")
       .addField(":inbox_tray: Wejście", "```"+args.join(" ")+"```")
