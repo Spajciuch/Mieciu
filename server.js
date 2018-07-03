e8c70392caa98c4d36b9d501d7f9e238a6b893a0
Sprite1234 <37411789+Sprite1234@users.noreply.github.com>
2018-07-03 12:42:01 +0200

    Info

--- server.js
+++ server.js
@@ -468,7 +468,7 @@ if(command == 'avatar'){
    .setAuthor("Informacje o bocie")
    .setColor(config.embed_color)
    .addField("Bot obsługuje ", `${client.users.size} osób, ${client.channels.size} kanałów, ${client.guilds.size} serwerów`)
-   .addField("Informacje o systemie", `**Bot działa na:** ${sys().name}\n**System:** ${sys().osystem}\n**Procesor:** ${sys().cpu} (${sys().arch}) \n**Pamięć RAM:** ${sys().rma} GB (wolna: ${sys().freeram} GB)\n**Node:** ${sys().node}`)
+   .addField("Informacje o systemie", `**Bot działa na:** ${sys().name}\n**System:** ${sys().osystem}\n**Procesor:** ${sys().cpu} (${sys().arch}) \n**Pamięć RAM:** ${sys().ram} GB (wolna: ${sys().freeram} GB)\n**Node:** ${sys().node}`)
    .addField("Ścieżka do pliku", `**Bot znajduje się w folderze:** ${__dirname}\n**Plik Główny:** ${__filename}`)
    .addField("Bot został włączony",` ${day} o ${time}`)
    message.channel.send({embed: info})
