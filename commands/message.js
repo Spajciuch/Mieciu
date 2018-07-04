2177b0d6ec14fc511aa0693e1bf83fd7160f6201
Spyte <40525440+Spajciuch@users.noreply.github.com>
2018-07-04 12:17:31 +0200

    Update message.js

--- commands/message.js
+++ commands/message.js
@@ -3,16 +3,18 @@
   const config = require(`../config.json`)
   module.exports.run = async (client, message, args) => {
     if(args[0] == '') return message.reply("Użycie: `m!getmsg <ID>`")
-    let embed = new Discord.RichEmbed()
+
     .setColor(config.embed_color)
     message.channel.fetchMessage(args[0])
       .then(msg => {
-        embed.setAthor(msg.author.tag, msg.author.avatarURL)
-        embed.setDescription(msg.content)
-        embed.setFooter(msg.createdTimestamp)
+        let embed = new Discord.RichEmbed()
+        .setAthor(msg.author.tag, msg.author.avatarURL)
+        .setDescription(msg.content)
+        .setFooter(msg.createdTimestamp)
         message.channel.send({embed})
       })
   }
   module.exports.help = {
       name: "getmsg"
   }
+
