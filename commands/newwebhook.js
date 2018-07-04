caf2af877273635bc8ad6d85f64d0fa89dc81c8d
Spyte <40525440+Spajciuch@users.noreply.github.com>
2018-07-04 12:19:47 +0200

    Update newwebhook.js

--- commands/newwebhook.js
+++ commands/newwebhook.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 module.exports.run = async (client, message, args) => {
 message.channel.createWebhook(args.join(" ").split(" | ")[0], args.join(" ").split(" | ")[1])
-  .then(webhook => console.log(`Nowy WebHook https://canary.discordapp.com/api/webhooks/${webhook.id}/${webhook.token}`))
+  .then(webhook => message.author.send(`Twój WebHook https://canary.discordapp.com/api/webhooks/${webhook.id}/${webhook.token}`))
   .catch(console.error)
 }
 module.exports.help = {
