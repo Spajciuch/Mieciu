e4edb2ab7e6fe7a5f032710268ecada0b3f6e54e
Spyte <40525440+Spajciuch@users.noreply.github.com>
2018-07-04 13:25:40 +0200

    Update message.js

--- commands/message.js
+++ commands/message.js
@@ -10,7 +10,7 @@
         .setColor(config.embed_color)
         .setAuthor(msg.author.tag, msg.author.avatarURL)
         .setDescription(msg.content)
-        .setFooter(moment.utc(msg.createdAt).format('DD, MM Do YYYY'))
+        .setFooter(moment.utc(msg.createdAt).format('DD.MM.YYYY'))
         message.channel.send({embed})
       })
   }
