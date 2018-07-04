6f2ad48ba088cff1d3945dcc43a71904aa743939
Sprite1234 <37411789+Sprite1234@users.noreply.github.com>
2018-07-04 11:51:28 +0200

    Poprawka webhooka

--- commands/newwebhook.js
+++ commands/newwebhook.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 module.exports.run = async (client, message, args) => {
-channel.createWebhook(args.join(" ").split(" | ")[0], args.join(" ").split(" | ")[1])
+message.channel.createWebhook(args.join(" ").split(" | ")[0], args.join(" ").split(" | ")[1])
   .then(webhook => console.log(`Nowy WebHook https://canary.discordapp.com/api/webhooks/${webhook.id}/${webhook.token}`))
   .catch(console.error)
 }
