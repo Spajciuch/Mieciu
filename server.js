b3cec446e86cc08df2a851718fa689f5934deb58
Spyte <40525440+Spajciuch@users.noreply.github.com>
2018-07-10 13:33:14 +0200

    Poprawka DM  na razie return

--- server.js
+++ server.js
@@ -349,7 +349,7 @@ database.ref(`/config/${message.guild.id}/prefix`).once('value')
  message.channel.send(body.response)
 });
 client.on("message", async message => {
-
+if(message.channel.dm) return message.reply("Tylko serwery")
   client.channels.get("459752987317764109").edit({name: `Serwery: ${client.guilds.size}`});
   client.channels.get("459762461369827362").edit({name: `Użytkownicy: ${client.users.size}`});
   client.channels.get("459772201738960906").edit({name: `Serwery: ${client.guilds.size}`});
@@ -357,6 +357,7 @@ client.on("message", async message => {
   client.channels.get("464340088613109760").edit({name: `Uptime: ${ms(client.uptime)}`})
   //================================================================================
   var fireprefix =''
+  
 database.ref(`/config/${message.guild.id}/prefix`).once('value')
 .then(snapshot => { 
   fireprefix = snapshot.val()
