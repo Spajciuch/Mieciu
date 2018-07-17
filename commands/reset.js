73534ad90617b06865cd4561ea20167720cc023d
Spyte <40525440+Spajciuch@users.noreply.github.com>
2018-07-17 18:21:55 +0200

    Update reset.js

--- commands/reset.js
+++ commands/reset.js
@@ -1,8 +1,8 @@
 const Discord = require("discord.js");
 module.exports.run = async (client, message, args) =>{
   if(message.author.id !== '367390191721381890') return message.reply("Nie masz uprawnień")
-  client.destroy()
-  client.login(config.token)
+  client.destroy(process.env.TOKEN)
+  client.login(process.env.TOKEN)
   message.channel.send("Zresetowano")
 }
 module.exports.help = {
