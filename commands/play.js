c56781b50cf49b0cb403e7f88fa3551f92bb635f
Spyte <40525440+Spajciuch@users.noreply.github.com>
2018-07-27 14:38:05 +0200

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -3,12 +3,18 @@ const config = require(`../config.json`)
 module.exports.run = async (client, message, args) => {
 message.member.voiceChannel.join()
  .then(connection => {
+	const { YTSearcher } = require('ytsearcher');
+const searcher = new YTSearcher('AIzaSyCOHw263Gs7JDtkYjsP_Hc50WLoOrk4G00');
+let result=  await searcher.search(args.join(" "), { type: 'video' });
+
+console.log(result.first);
+console.log(result.first.url);
 console.log('Connected!')
 const ytdl = require('ytdl-core');
 const streamOptions = { seek: 0, volume: 1 };
 const broadcast = client.createVoiceBroadcast();
 
- const stream = ytdl(args.join(" "), { filter : 'audioonly' });
+ const stream = ytdl(result.first.url, { filter : 'audioonly' });
  broadcast.playStream(stream);
  const dispatcher = connection.playBroadcast(broadcast);
  })
