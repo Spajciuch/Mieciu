e01620ebc987eaea7a44080f51f6a21bfa77d627
Spyte <40525440+Spajciuch@users.noreply.github.com>
2018-08-12 11:21:46 +0200

    Update bank.js

--- commands/bank.js
+++ commands/bank.js
@@ -7,10 +7,12 @@ database.ref(`/economy/${message.guild.id}/${message.author.id}/inventory`).once
     .then(async  inventory=>  {
         database.ref(`/economy/${message.guild.id}/${message.author.id}/bucks`).once("value")
     .then(async  bucks=>  {
+		var dol = bucks.val()
+		if(dol == null) dol = 0
         let embed = new Discord.RichEmbed()
         .setColor(config.embed_color)
         .setAuthor(`Konto ${message.author.username}#${message.author.discriminator}`, message.author.displayAvatarURL)
-        .addField("Stan konta", bucks.val() + "M$")
+        .addField("Stan konta", dol + "M$")
         message.channel.send({embed})
     })
 })
