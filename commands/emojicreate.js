9905afe4b0a63c23255f325a8299eae4e1c63e9e
Spyte <40525440+Spajciuch@users.noreply.github.com>
2018-08-13 16:59:39 +0200

    Update emojicreate.js

--- commands/emojicreate.js
+++ commands/emojicreate.js
@@ -1,8 +1,6 @@
 const Discord = require('discord.js')
 module.exports.run = async (client, message, args) => {
-   if(!message.member.roles.some(r=>["Admin","Administrator", "Moderator", "podkomisarze w Hyrule", "agent FBI w Hyrule", "właściciele", "Administracja"].includes(r.name)))
-      return message.reply("Nie masz uprawnień")
-    return message.reply("Nie masz uprawnień")
+  if (!message.member.hasPermission("MANAGE_EMOJIS")) return message.reply("Nie masz uprawnień");
     message.guild.createEmoji(args[0], args[1]) .then(emoji => console.log(`Utworzono nowe emoji na serwerze ${message.guild.name} o nazwie: ${emoji.name}`)) .catch(console.error);
 }
 module.exports.help = {
