74a8b2d99c1ddb7a0019c741a36268c9df1404a4
Spyte <40525440+Spajciuch@users.noreply.github.com>
2018-08-15 16:10:17 +0200

    Add files via upload

---
+++ commands/warn.js
@@ -0,0 +1,29 @@
+const Discord = require("discord.js");
+const config = require(`../config.json`)
+module.exports.run = async (client, message, args) => {
+	if (!message.member.hasPermission("BAN_MEMBERS")) return message.reply("Nie masz uprawnień");
+	var firebase = require('firebase')
+	var database = firebase.database()
+	let member = message.mentions.members.first();
+	if(args[1] == 'clear') {
+			database.ref(`warns/${message.guild.id}/${member.id}`).set({
+				warns:0
+			})
+	} else { database.ref(`warns/${message.guild.id}/${member.id}/warns`).once("value")
+		.then(async warns => {
+			var warn = warns.val()
+			if(warn == null) warn = 0
+			database.ref(`warns/${message.guild.id}/${member.id}`).set({
+				warns:warn+1
+			})
+			if(warn == 4){
+				 if(!member.bannable) return message.guild.owner.send(`${member} dostał 5 ostrzeżeń, ale nie mogę go zbanować`)
+				member.ban("Dostał 5 ostrzeżeń")
+			}
+		})
+	}
+}
+module.exports.help = {
+	name: "warn",
+	category:"admin"
+}
