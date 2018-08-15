e6f205156b620a12712685555c9917885eb57623
Spyte <40525440+Spajciuch@users.noreply.github.com>
2018-08-15 17:16:02 +0200

    Add files via upload

---
+++ commands/fmk.js
@@ -0,0 +1,26 @@
+const Discord = require("discord.js");
+const google = require("google");
+var config = require('../config')
+var fs = require('fs')
+module.exports.run = async (client, message, args) => {
+ let replies = ['MARRY :ring:', 'KILL :bomb:', 'FUCK :ok_hand:'];
+    let result = Math.floor(Math.random() * replies.length);
+
+    let makifuembed = new Discord.RichEmbed()
+
+        .setDescription(`**${args[0]} został wybrany przez <@${message.author.id}>**`)
+        .setColor(config.embed_color)
+        .addField('Wybrałeś:', replies[result])
+        .setFooter('Fuck, Marry, Kill!')
+        .setTimestamp();
+
+    if (!message.mentions.users.first()) return message.channel.send(`<@${message.author.id}>, please mention a user you wanna choose!`).then(msg => {
+        msg.delete(3000)
+    });
+
+    message.channel.send(makifuembed);
+}
+module.exports.help = {
+  name: "fmk",
+  category:"util"
+}
