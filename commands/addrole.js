8298d186a206ab94c7a00f83afe95e069b5ebdf8
Spyte <40525440+Spajciuch@users.noreply.github.com>
2018-08-18 12:32:20 +0200

    Addrole i Removerole

---
+++ commands/addrole.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js");
+const config = require(`../config.json`)
+module.exports.run = async (client, message, args) => {
+	var firebase = require('firebase')
+  var database = firebase.database()
+  await database.ref(`/config/${message.guild.id}/util`).once('value')
+  .then(async pingi => {
+    if(pingi.val() == false) return message.reply('Komenda jest wyłączona');
+	let member = message.mentions.members.first();
+	if (!message.member.hasPermission("MANAGE_GUILD")) return message.reply("Nie masz uprawnień");
+	var name =  args.join(" ").slice(22)
+	var role = message.guild.roles.find('name',name)
+	if(!role) return message.reply("Nie ma takiej roli")
+	member.addRole(role).then(rola => {
+		message.channel.send(`Dodano rolę ${role} dla ${member}`)
+	})
+})
+}
+module.exports.help = {
+	name: "addrole",
+	category:"admin"
+}
